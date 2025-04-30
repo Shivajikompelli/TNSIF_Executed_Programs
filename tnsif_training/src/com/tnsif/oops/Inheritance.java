c16e21b6ca665b733b00130b520dd571c802e670
shivaji.kompelli <shivajikompelli@gmail.com>
2025-04-30 12:34:32 +0530

    oops concepts

--- tnsif_training/src/com/tnsif/oops/Inheritance.java
+++ tnsif_training/src/com/tnsif/oops/Inheritance.java
@@ -1,13 +1,14 @@
 package com.tnsif.oops;
-
-//public class Inheritance {
+//inheritance demo
 	
+// creating a class for Animals
 	class Animals{
 		void eat() {
 			System.out.println("Every Animal Eats");
 		}
 
 	}
+//creating a class dog and acquiring properties of Animals using extends keyword
 	class Dog extends Animals{
 		void sound() {
 			System.out.println("the Dog barks");
@@ -18,12 +19,10 @@ package com.tnsif.oops;
 		
 	
 	public static void main(String[] args) {
-		
+		//creating instance
 		Dog ob=new Dog();
 		ob.eat();
 		ob.sound();
-		
-		
 	}
 	}
 
