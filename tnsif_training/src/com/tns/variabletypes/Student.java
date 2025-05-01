8e05ea7a3fd4d1c14c7f313f02972e5165600574
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-01 07:55:46 +0530

    Create Student.java

---
+++ tnsif_training/src/com/tns/variabletypes/Student.java
@@ -0,0 +1,12 @@
+package com.tns.variabletypes;
+
+//Demo for static variable
+public class Student {
+	int sid;
+	String sname;
+	static String collegename="Sri indu";
+	 void display() {
+		 System.out.println("The details of the student"
+				 		+sid+","+sname+","+collegename);
+	 }
+}
