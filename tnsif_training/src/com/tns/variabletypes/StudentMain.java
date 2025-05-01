3bd7a37b8cc6d32d11e186684a436866b14b600d
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-01 07:56:24 +0530

    Create StudentMain.java

---
+++ tnsif_training/src/com/tns/variabletypes/StudentMain.java
@@ -0,0 +1,24 @@
+package com.tns.variabletypes;
+
+public class StudentMain {
+	public static void main(String[] args) {
+     Student ob=new Student();
+     ob.sid=101;
+     ob.sname="kavya";
+     ob.display();
+    
+     Student o=new Student();
+     o.sid=102;
+     o.sname="kavitha";
+     o.display();
+    
+     Student b=new Student();
+     b.sid=103;
+     b.sname="Bobby";
+     b.display();
+		
+		
+		
+		
+	}
+}
