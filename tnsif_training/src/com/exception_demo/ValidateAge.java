ef8b542cd32afc3340ff304c033f8bc0f9d05063
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-11 11:41:17 +0530

    ValidateAge

---
+++ tnsif_training/src/com/exception_demo/ValidateAge.java
@@ -0,0 +1,15 @@
+package com.tnsif.exceptiondemo;
+
+public class ValidateAge {
+	
+	
+	public static void validateAge(int age) throws MyException,ArithmeticException {
+		if(age<0) {
+			throw new MyException("The entered age is not valid");
+		}
+		else {
+			System.out.println("The age is valid");
+		}
+	}
+	
+}
