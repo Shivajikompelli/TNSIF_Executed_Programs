c0bae55f716ba9b7303545c6eb307ce9047184b0
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-11 11:37:32 +0530

    Create ExceptionMain.java

---
+++ tnsif_training/src/com/exception_demo/ExceptionMain.java
@@ -0,0 +1,7 @@
+package com.tnsif.exception_demo;
+
+public class ExceptionMain {
+	public static void main(String[] args) throws ArithmeticException, MyException {
+     ValidateAge.validateAge(12);	
+	}
+}
