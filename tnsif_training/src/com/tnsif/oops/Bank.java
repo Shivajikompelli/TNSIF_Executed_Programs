5a89f6dff1d375ee1b2acc3f3ce3680384075db4
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-12 11:32:15 +0530

    interface

---
+++ tnsif_training/src/com/tnsif/oops/Bank.java
@@ -0,0 +1,12 @@
+package com.tnsif.oops;
+//interface demo
+public interface Bank {
+ 
+	//abstract methods
+	double DEPOSIT_LIMIT=25000;
+	void withdraw(double amount);
+	void deposit(double amount);
+	
+}	
+	
+
