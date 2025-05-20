74f712b7f7cef7b67dfff9a8b81f80b8e0cb91e9
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-20 15:16:15 +0530

    generics

---
+++ tnsif_training/src/com/generics/GenericsMethodMain.java
@@ -0,0 +1,18 @@
+package com.tnsif.generics;
+
+public class GenericMethodMain {
+
+	public static void main(String[] args) {
+		
+		
+		Integer[] intarray= {1,2,3};
+		String[] stringarray= {"Allen","dolly","candy"};
+		
+		
+		//call the method(static)
+		
+		GenericMethodDemo.displayArray(stringarray);
+
+	}
+
+}
