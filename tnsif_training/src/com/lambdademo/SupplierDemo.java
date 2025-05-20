7027f5eefb3eb49012b69737a6ec9a72fc9292c3
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-20 15:03:54 +0530

    SupplierDemo

---
+++ tnsif_training/src/com/lambdademo/SupplierDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.lambdademo;
+
+import java.util.function.Supplier;
+
+public class SupplierDemo {
+	public static void main(String[] args) {
+		
+		Supplier<Double> sup=()->Math.random();
+		System.out.println(sup.get());
+	}
+
+}
