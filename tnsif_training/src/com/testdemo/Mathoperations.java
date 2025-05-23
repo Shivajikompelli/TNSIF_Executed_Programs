73c7d22bd44471445875b85005dc8376bdd65b80
Shivajikompelli <shivajikompelli@gmail.com>
2025-05-23 11:02:00 +0530

    test

---
+++ tnsif_training/src/com/testdemo/Mathoperations.java
@@ -0,0 +1,13 @@
+package com.tnsif.testdemo;
+
+public class MathOperations {
+	public static boolean positveNumber(int num) {
+		if(num>0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
